a768aae9c2a8c7716f50c6c2e134121ad87948c2
Iyehvah <Iyehvah@users.noreply.github.com>
2020-01-25 09:55:10 -0600

    Redid my navigation and still cant get rid of the underlined links.. rip.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,12 @@
 import React from "react";
 
 const Navigation = () => {
-    return(
+    return (
         <div className="navigation">
-                <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#portfolio">Portfolio</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                </ul>
+            <a href="#" className="atags">Home</a>
+            <a href="#" className="atags">About</a>
+            <a href="#" className="atags">Portfolio</a>
+            <a href="#" className="atags">Contact</a>
         </div>
     )
 }
