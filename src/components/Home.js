498d569facfff4549d22c6ff9ab6ffb3d7802a07
Iyehvah <Iyehvah@users.noreply.github.com>
2020-01-25 09:20:30 -0600

    Adding some styling will probly change in the future.. also cant figure out why my atags still have that underline. Might have to redu navigation.

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,9 @@ import ProfilePicture from "../images/Prof-Pic2.jpg"
 
 const Home = () => {
     return (
-        <div className="container">
+        <div className="home-container">
                 <span className="image fit"><img className="hvr-grow" src={ProfilePicture} /></span>
+                <div className="headline"><h3>Front-End Web Developer looking to better the world one step at a time.</h3></div>
         </div>
     )
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,15 +2,13 @@ import React from "react";
 
 const Navigation = () => {
     return(
-        <div className="container">
-            <nav className="nav">
+        <div className="navigation">
                 <ul>
-                    <li><a href="#top">Top</a></li>
-                    <li><a href="#work">Work</a></li>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
                     <li><a href="#portfolio">Portfolio</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
-            </nav>
         </div>
     )
 }
